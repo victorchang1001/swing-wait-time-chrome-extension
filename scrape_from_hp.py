4b0e6733aea62a135280c3f06f774f67e1786ac0
Victor Chang <victortwchang@gmail.com>
2022-10-18 00:31:58 +0900

    scrape_from_hp.py now prints tabulated results

--- scrape_from_hp.py
+++ scrape_from_hp.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+# import csv
+from tabulate import tabulate
 
 URL = "https://www.tokyu-sports.com/golf/himonya.html"
 page = requests.get(URL)
@@ -11,17 +12,17 @@ results = soup.find(id="pbBlock17580224")
 wait_times = results.find_all("span", class_="floor_info")
 last_update = results.find("span", class_="order-date")
 
-print(last_update.text)
-
-fields = ["階数", "待ち時間"]
 rows = []
 for idx in range(3):
     row = [str(idx+1) + "階", wait_times[idx].text]
     rows.append(row)
-    print(row)
+    # print(row)
 
-with open('result.csv', 'w') as f:
-    write = csv.writer(f)
+# with open('result.csv', 'w') as f:
+#     write = csv.writer(f)
 
-    write.writerow(fields)
-    write.writerows(rows)
+#     write.writerow(fields)
+#     write.writerows(rows)
+
+print("<<-- " + last_update.text + " -->>")
+print(tabulate(rows, headers=["階数", "待ち時間"], stralign="center"))
